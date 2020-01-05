7bb03ca329f732ab81e648c973793a3c4d26e1ef
szaboandrea <szaboandi003@gmail.com>
2020-01-06 00:54:30 +0200

    Recycler view

--- app/src/main/java/com/example/pedapp/Fragments/StudentHomepageFragment.java
+++ app/src/main/java/com/example/pedapp/Fragments/StudentHomepageFragment.java
@@ -3,6 +3,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,14 +14,21 @@ import android.widget.Button;
 import android.widget.Spinner;
 
 import com.example.pedapp.Activity.MainActivity;
+import com.example.pedapp.Classes.Test;
+import com.example.pedapp.Classes.TestAdapter;
 import com.example.pedapp.Database.SQLiteTest;
 import com.example.pedapp.R;
 
 import java.util.ArrayList;
 
 public class StudentHomepageFragment extends Fragment {
-    private Button buttonLogOut, buttonResult;
+    private Button buttonLogOut, buttonTestFill;
     private ArrayList<String> groupList = new ArrayList<>();
+    private ArrayList<Test> tests = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private TestAdapter testAdapter;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,27 +39,30 @@ public class StudentHomepageFragment extends Fragment {
         SQLiteTest database = new SQLiteTest(getActivity());
         groupList = database.getAllGroupName();
         buttonLogOut = view.findViewById(R.id.ButtonLogout);
-//        buttonResult = view.findViewById(R.id.ButtonResults);
+        buttonTestFill = view.findViewById(R.id.ButtonTestFill);
+        recyclerView = view.findViewById(R.id.recyclerViewTest);
+
         logout();
-//        results();
-        Spinner spinnerTest = view.findViewById(R.id.SpinnerStudent);
+        final Spinner spinnerTest = view.findViewById(R.id.SpinnerStudent);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.student_homepage_spinner, groupList);
         adapter.setDropDownViewResource(R.layout.student_homepage_spinner);
         spinnerTest.setAdapter(adapter);
 
+        buttonTestFill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String selectedItem = spinnerTest.getSelectedItem().toString();
+                SQLiteTest database = new SQLiteTest(getActivity());
+                tests = database.getTestOneGroup(selectedItem);
+                layoutManager = new LinearLayoutManager(getContext());
+                testAdapter = new TestAdapter(getContext(), tests);
+                recyclerView.setLayoutManager(layoutManager);
+                recyclerView.setAdapter(testAdapter);
+            }
+        });
+
         return view;
     }
-
-//    public void results(){
-//        buttonResult.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                MainActivity.mFragmentManager.beginTransaction()
-//                        .replace(R.id.fragmentContainer, new ResultFragment(),null)
-//                        .commit();
-//            }
-//        });
-//    }
 
     /**
      * if you click the "kijelentkezes" button, you can log out
